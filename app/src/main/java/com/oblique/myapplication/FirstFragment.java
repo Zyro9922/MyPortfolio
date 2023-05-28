595290c53f2bc0a5c0aebcaf06374765419fac73
Ali Hasan <alihasan9922@gmail.com>
2023-05-28 19:01:28 +0530

    Removed pre-code

--- app/src/main/java/com/oblique/myapplication/FirstFragment.java
+++ app/src/main/java/com/oblique/myapplication/FirstFragment.java
@@ -16,7 +16,7 @@ private FragmentFirstBinding binding;
 
     @Override
     public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
+            @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
 
